335f4100debb749ef800a857dbf9d00f75a88726
Balykov Semyon <balykov.semen2010@mail.ru>
2026-09-18 20:54:28 +0300

    [TASK-1] Initial commit

---
+++ semyon.balykov/task-1/cmd/service/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+	var b int
+	var operation string
+
+	_, err := fmt.Scan(&a)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+
+	_, err = fmt.Scan(&b)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+
+	fmt.Scan(&operation)
+	switch operation {
+	case "+":
+		fmt.Println(a + b)
+	case "-":
+		fmt.Println(a - b)
+	case "*":
+		fmt.Println(a * b)
+	case "/":
+		if b == 0 {
+			fmt.Println("Division by zero")
+			return
+		}
+		fmt.Println(a / b)
+	default:
+		fmt.Println("Invalid operation")
+	}
+}
---
+++ semyon.balykov/task-1/go.mod
@@ -0,0 +1,3 @@
+module github.com/sbalyk/task-1
+
+go 1.27.1
